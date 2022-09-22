41695f3b7b0b7e365dfa9d7b9a10c36980f83717
Leon7422 <y.kurduban98@gmail.com>
2022-09-22 22:42:56 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+galleryItemsCreate(galleryItems);
+
+function galleryItemsCreate(array) {
+  const imagesPrewievTable = array
+    .map((image) => {
+      return `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`;
+    })
+    .join("");
+  galleryBox.innerHTML = imagesPrewievTable;
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionClass: "centered-text",
+});
